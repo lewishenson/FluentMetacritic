2680454fda31eb6cdc1f105e595775a53331770c
Lewis Henson <lewis.henson@gmail.com>
2016-11-14 14:03:57 +0000

    Set UserAgent on HTTP Client.

--- FluentMetacritic/Net/HttpClientWrapper.cs
+++ FluentMetacritic/Net/HttpClientWrapper.cs
@@ -5,7 +5,13 @@ namespace FluentMetacritic.Net
 {
     public class HttpClientWrapper : IHttpClient
     {
-        private static readonly HttpClient Client = new HttpClient();
+        private static readonly HttpClient Client;
+
+        static HttpClientWrapper()
+        {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.11 Safari/537.36");
+        }
 
         public async Task<string> GetContentAsync(string address)
         {
